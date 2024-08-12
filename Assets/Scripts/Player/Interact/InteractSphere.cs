9d2c983c50efee62f94aa41bfb227bf6343d1cd8
Jacques-afk <s10243606@connect.np.edu.sg>
2024-08-12 20:34:40 +0800

    GRAH

--- Assets/Scripts/Player/Interact/InteractSphere.cs
+++ Assets/Scripts/Player/Interact/InteractSphere.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,6 +6,7 @@ public class InteractSphere : MonoBehaviour
 {
     private List<GameObject> interactablesInRange = new List<GameObject>();
     private PlayerInteract playerInteract;
+    private float duration = 2f;
 
     private void Awake() {
         playerInteract = GetComponentInParent<PlayerInteract>();
@@ -42,7 +44,54 @@ public class InteractSphere : MonoBehaviour
             }
         }
 
+
         Debug.Log(closestInteractable);
+
+        // Getting alpha of prompt to animate
+        Transform UICanvas = closestInteractable.transform.Find("Canvas");
+        CanvasGroup alphaSlider = UICanvas.GetComponent<CanvasGroup>();
+        StartCoroutine(IncreaseAlpha(alphaSlider, alphaSlider.alpha));
+
+
         return closestInteractable;
     }
+
+
+
+
+
+    private IEnumerator IncreaseAlpha(CanvasGroup alphaSlider, float currentAlpha)
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            yield return new WaitForSeconds(0.000000001f);
+            elapsedTime += Time.deltaTime;
+            alphaSlider.alpha = Mathf.Lerp(currentAlpha, 1f, elapsedTime / duration);
+            yield return null; // Wait for the next frame
+        }
+
+        // Ensure the final value is set to the endValue
+        alphaSlider.alpha = 1f;
+    }
+
+    private IEnumerator DecreaseAlpha(CanvasGroup alphaSlider, float currentAlpha)
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            yield return new WaitForSeconds(0.000000001f);
+            elapsedTime += Time.deltaTime;
+            alphaSlider.alpha = Mathf.Lerp(currentAlpha, 0f, elapsedTime / duration);
+            // Optionally, you can do something with currentValue here
+            yield return null; // Wait for the next frame
+        }
+
+        // Ensure the final value is set to the endValue
+        alphaSlider.alpha = 0f;
+    }
+
+
 }
